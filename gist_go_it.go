6bc3ffca7d29de0ba22cd00000c80e4b2743143e
David Tomberlin <siyegen@gmail.com>
2014-04-17 22:25:44 -0700

    add base http get logic for gists

--- gist_go_it.go
+++ gist_go_it.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/sendgrid/sendgrid-go"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -14,29 +17,63 @@ type EmailMessage struct {
 	Text     string
 }
 
+type GistResponse struct {
+	Filename string
+	Content  string
+}
+
+type GistFiles map[string]*GistResponse
+
+type XXResponse struct {
+	Files GistFiles
+}
+
 func main() {
-	sg_user := GetEnvOrExit("SG_USER")
-	sg_key := GetEnvOrExit("SG_KEY")
+	// sg_user := GetEnvOrExit("SG_USER")
+	// sg_key := GetEnvOrExit("SG_KEY")
 
-	message := &EmailMessage{
-		ToAddr:   "siyegen@gmail.com",
-		FromAddr: "siyegen@gmail.com",
-		Subject:  "Testing Testing",
-		Text:     "Just some go",
-	}
+	// message := &EmailMessage{
+	// 	ToAddr:   "siyegen@gmail.com",
+	// 	FromAddr: "siyegen@gmail.com",
+	// 	Subject:  "Testing Testing",
+	// 	Text:     "Just some go",
+	// }
 
-	SendEmail(message, sg_user, sg_key)
+	// SendEmail(message, sg_user, sg_key)
+
+	// gh_user := GetEnvOrExit("GH_USER")
+	gh_gist := GetEnvOrExit("GH_GIST")
+	gh_key := GetEnvOrExit("GH_KEY")
+
+	url := fmt.Sprintf("https://api.github.com/gists/%s", gh_gist)
+	fmt.Println(GetGist(url, gh_key))
 }
 
-func GetEnvOrExit(name string) string {
-	log.Printf("Accessing %s", name)
-	value := os.Getenv(name)
+func GetGist(url, key string) *GistResponse {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("authorization token", key)
 
-	if value == "" {
-		log.Fatalf("%s was empty!", name)
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatalf("Error with get %s", err)
 	}
-	return value
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error with get %s", err)
+	}
+
+	var gist_resp XXResponse
+	err = json.Unmarshal(body, &gist_resp)
+	if err != nil {
+		log.Fatalf("Error with get %s", err)
+	}
+
+	return gist_resp.Files["remind_todo"]
 }
+
 func SendEmail(sgmessage *EmailMessage, sg_user, sg_key string) {
 	sg := sendgrid.NewSendGridClient(sg_user, sg_key)
 	message := sendgrid.NewMail()
@@ -50,4 +87,14 @@ func SendEmail(sgmessage *EmailMessage, sg_user, sg_key string) {
 	} else {
 		fmt.Println(r)
 	}
+}
+
+func GetEnvOrExit(name string) string {
+	log.Printf("Accessing %s", name)
+	value := os.Getenv(name)
+
+	if value == "" {
+		log.Fatalf("%s was empty!", name)
+	}
+	return value
 }
