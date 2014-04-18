e48e486e7d3b0cb87663bc793b6a10120f015c44
David Tomberlin <siyegen@gmail.com>
2014-04-17 21:20:19 -0700

    added simple email message

--- gist_go_it.go
+++ gist_go_it.go
@@ -7,24 +7,25 @@ import (
 	"os"
 )
 
+type EmailMessage struct {
+	ToAddr   string
+	FromAddr string
+	Subject  string
+	Text     string
+}
+
 func main() {
 	sg_user := GetEnvOrExit("SG_USER")
 	sg_key := GetEnvOrExit("SG_KEY")
 
-	fmt.Println("Hello", sg_user, sg_key)
-	sg := sendgrid.NewSendGridClient(sg_user, sg_key)
-	message := sendgrid.NewMail()
-	message.AddTo("siyegen@gmail.com")
-	message.AddToName("Moo Moo")
-	message.AddSubject("SendGrid Testing")
-	message.AddText("WIN")
-	message.AddFrom("siyegen@gmail.com")
-
-	if r := sg.Send(message); r == nil {
-		fmt.Println("Email sent!")
-	} else {
-		fmt.Println(r)
+	message := &EmailMessage{
+		ToAddr:   "siyegen@gmail.com",
+		FromAddr: "siyegen@gmail.com",
+		Subject:  "Testing Testing",
+		Text:     "Just some go",
 	}
+
+	SendEmail(message, sg_user, sg_key)
 }
 
 func GetEnvOrExit(name string) string {
@@ -35,4 +36,18 @@ func GetEnvOrExit(name string) string {
 		log.Fatalf("%s was empty!", name)
 	}
 	return value
+}
+func SendEmail(sgmessage *EmailMessage, sg_user, sg_key string) {
+	sg := sendgrid.NewSendGridClient(sg_user, sg_key)
+	message := sendgrid.NewMail()
+	message.AddTo(sgmessage.ToAddr)
+	message.AddSubject(sgmessage.Subject)
+	message.AddText(sgmessage.Text)
+	message.AddFrom(sgmessage.FromAddr)
+
+	if r := sg.Send(message); r == nil {
+		fmt.Println("Email sent!")
+	} else {
+		fmt.Println(r)
+	}
 }
